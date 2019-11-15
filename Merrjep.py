d958884daa340ee46144f9eb987019a12b026a14
Lirim Krosa <araptarap@gmail.com>
2019-11-15 00:32:25 +0000

    Update Merrjep.py

--- Merrjep.py
+++ Merrjep.py
@@ -4,29 +4,29 @@ import csv
 import requests
 from bs4 import BeautifulSoup    
 import datetime
+import re
 
 
-end_page_num = int(input("Shtyp Numrin e Faqeve: "))
+pagenum = int(input("Shtyp Numrin e Faqeve: "))
 qyteti = input("Ne cilin qytet doni te kerkoni?: ")
 linku = "https://www.merrjep.com/shpalljet/{}".format(qyteti)
 
-filename = "Merrjep_" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")+".csv"
+filename = "Merrjep_" + datetime.datetime.now().strftime("%Y-%m-%d-%H-%M")+".txt"
 with open(filename, "w+", encoding='utf-8') as f:
 
     writer = csv.writer(f)
-    writer.writerow(["Descriere","Data"])
     i = 1
-    while i <= end_page_num:
+    while i <= pagenum:
 
         r = requests.get(linku,"?Page={}".format(i))
-        content = r.content
+        #content = r.content
 
         soup = BeautifulSoup(r.text, "html5lib")
         x = soup.find_all("div", {'class': 'row row-listing'})
 
         for thumbnail in x:
-            descriere = thumbnail.find('a')['href']
-            strs = descriere.replace('/shpallja/', 'https://merrjep.com/shpallja/')
+            urlpostimet = thumbnail.find('a')['href']
+            strs = urlpostimet.replace('/shpallja/', 'https://merrjep.com/shpallja/')
             writer.writerow([strs])
-            print(strs)
+            print(strs)            
         i += 1
